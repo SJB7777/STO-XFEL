f8f39ed4ed9791812224178b89110b2cc1e04309
esakyong <esakyong1866@naver.com>
2024-08-05 17:05:29 +0900

    add: gaussian fitting

--- src/analysis/mean_data_processor.py
+++ src/analysis/mean_data_processor.py
@@ -1,8 +1,10 @@
 from roi_rectangle import RoiRectangle
 import numpy as np
 import pandas as pd
-
 from scipy.ndimage import rotate
+from scipy.optimize import curve_fit
+
+from utils.math_util import gaussian
 
 import numpy.typing as npt
 
@@ -34,7 +36,7 @@ class MeanDataProcessor:
     def pon_subtract_by_poff(self):
         return np.maximum(self.pon_images - self.poff_images, 0)
     
-    def _roi_center_of_masses(self, roi_rect: RoiRectangle, images: npt.NDArray):
+    def _roi_center_of_masses(self, roi_rect: RoiRectangle, images: npt.NDArray) -> tuple[npt.NDArray, npt.NDArray]:
         roi_images = roi_rect.slice(images)
         num_images, height, width = roi_images.shape
 
@@ -44,8 +46,42 @@ class MeanDataProcessor:
         x_centroids = np.sum(x_coords * roi_images, axis=(1, 2)) / total_mass
         y_centroids = np.sum(y_coords * roi_images, axis=(1, 2)) / total_mass
 
-        return np.stack((x_centroids, y_centroids), axis=-1)
+        return x_centroids, y_centroids
     
+    def _roi_gaussian(self, roi_rect: RoiRectangle, images: npt.NDArray) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
+        roi_images = roi_rect.slice(images)
+        num_images, height, width = roi_images.shape
+        
+        intensities = []
+        com_xs = []
+        com_ys = []
+
+        for image in roi_images:
+            max_y, max_x = np.unravel_index(np.argmax(image), image.shape)
+            
+            x: npt.NDArray = np.arange(0, width + 1)
+            y: npt.NDArray = np.arange(0, height + 1)
+
+            x_data = image.sum(axis=0)
+            y_data = image.sum(axis=1)
+
+            initial_guess_x = [x_data.max(), max_x, (np.max(x_data) - np.min(x_data)) / 6]
+            params_x, covar_x = curve_fit(gaussian, x, x_data, p0=initial_guess_x)
+
+            initial_guess_y = [y_data.max(), max_y, (np.max(y_data) - np.min(y_data)) / 6]
+            params_y, covar_y = curve_fit(gaussian, y, y_data, p0=initial_guess_y)
+
+            gaussian_a_x, gaussain_com_x, gaussian_sig_x = params_x
+            gaussian_a_y, gaussain_com_y, gaussian_sig_y = params_y
+
+            gaussian_a = np.sqrt(gaussian_a_x * gaussian_a_y)
+            intensities.append(gaussian_a)
+            com_xs.append(gaussain_com_x)
+            com_ys.append(gaussain_com_y)
+
+        return np.stack(intensities), np.stack(com_xs), np.stack(com_ys)
+
+
     def _roi_intensities(self, roi_rect: RoiRectangle, images: npt.NDArray):
         roi_images = roi_rect.slice(images)
 
@@ -55,18 +91,31 @@ class MeanDataProcessor:
         data_frames = []
 
         for name, roi_rect in named_roi_rects:
-            pon_com = self._roi_center_of_masses(roi_rect, self.pon_images)
-            poff_com = self._roi_center_of_masses(roi_rect, self.poff_images)
-            pon_intensity = self._roi_intensities(roi_rect, self.pon_images)
+
+            poff_com_x, poff_com_y = self._roi_center_of_masses(roi_rect, self.poff_images)
             poff_intensity = self._roi_intensities(roi_rect, self.poff_images)
+            poff_guassain_intensity, poff_gussian_com_x, poff_gussian_com_y = self._roi_gaussian(roi_rect, self.poff_images)
+
+            pon_com_x, pon_com_y = self._roi_center_of_masses(roi_rect, self.pon_images)
+            pon_intensity = self._roi_intensities(roi_rect, self.pon_images)
+            pon_guassain_intensity, pon_gussian_com_x, pon_gussian_com_y = self._roi_gaussian(roi_rect, self.pon_images)
 
             roi_df = pd.DataFrame(data={
-                "pon_com_x": pon_com.T[0],
-                "pon_com_y": pon_com.T[1],
-                "poff_com_x": poff_com.T[0],
-                "poff_com_y": poff_com.T[1],
+                "poff_com_x": poff_com_x,
+                "poff_com_y": poff_com_y,
+                "poff_intensity": poff_intensity,
+
+                "poff_guassain_intensity": poff_guassain_intensity,
+                "poff_gussian_com_x": poff_gussian_com_x,
+                "poff_gussian_com_y": poff_gussian_com_y,
+
+                "pon_com_x": pon_com_x,
+                "pon_com_y": pon_com_y,
                 "pon_intensity": pon_intensity,
-                "poff_intensity": poff_intensity
+
+                "pon_guassain_intensity": pon_guassain_intensity,
+                "pon_gussian_com_x": pon_gussian_com_x,
+                "pon_gussian_com_y": pon_gussian_com_y,
             })
             
             roi_df = roi_df.transpose()
@@ -82,19 +131,36 @@ class MeanDataProcessor:
         return data_df
     
 if __name__ == "__main__":
-    # file = "D:\\dev\\p_python\\xrd\\xfel_sample_data\\Npz_files\\run=0001_scan=0001.npz"
-    file = "D:\\dev\\p_python\\xrd\\xfel_sample_data\\Npz_files\\run=062\\scan=001\\run=062, scan=001.npz"
-    mdp = MeanDataProcessor(file, -45)
-    roi_rects = [RoiRectangle(0, 0, None, None), RoiRectangle(100, 200, 500, 600)]
-    names = ["total", "center"]
+    import os
+    from gui.roi import select_roi_by_run_scan
+    from utils.file_util import create_run_scan_directory
+    from cuptlib_config.palxfel import load_palxfel_config
+    from typing import Optional
+    config = load_palxfel_config("config.ini")
+
+    run_num: int = 1
+    scan_num: int = 1
+    comment: Optional[str] = None
+
+    npz_dir: str = config.path.npz_dir
+
+    file_name: str = f"run={run_num:0>4}_scan={scan_num:0>4}"
+    if comment is not None:
+        file_name += comment
+
+    npz_file = os.path.join(npz_dir, file_name + ".npz")
+    mdp = MeanDataProcessor(npz_file, -45)
+
+    roi_rect = select_roi_by_run_scan(run_num, scan_num)
+
+    roi_rects = [roi_rect]
+    names = ["center"]
     named_roi_rects = zip(names, roi_rects)
     data_df = mdp.analyze_by_rois(named_roi_rects)
+
+    image_dir = config.path.image_dir
+    data_dir = create_run_scan_directory(image_dir, run_num, scan_num)
+    data_file = os.path.join(data_dir, "data.csv")
+    data_df.to_csv()
     
-    import matplotlib.pyplot as plt
-    name = 'total'
-    
-    poff_images = mdp.poff_images
-    poff_image, pon_image = mdp.get_summed_image()
-    
-    pon_sub_poff = mdp.pon_subtract_by_poff()
     
