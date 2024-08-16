f4ce3c95a52daf18b26718a8c622bb02e16ef28b
SJB7777 <esakyong1866@naver.com>
2024-08-16 19:51:57 +0900

    Add: analyzer/loader.py NpzLoader

--- src/analyzer/loader.py
+++ src/analyzer/loader.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.io import loadmat
 import numpy.typing as npt
 
@@ -9,20 +10,16 @@ class MatLoader:
         self.images = images.swapaxes(1, 2)
 
 
+class NpzLoader:
+    def __init__(self, file: str):
+        self.data =  dict(np.load(file))
+
+
+
 if __name__ == "__main__":
     import os
     from src.config.config import load_config
     config = load_config()
-    mat_dir = config.path.mat_dir
-    file_name = "run=0143_scan=0001_poff"
-    mat_file = os.path.join(mat_dir, file_name + ".mat")
-    mat_loader = MatLoader(mat_file)
-    off_images = mat_loader.images
-
-    file_name = "run=0143_scan=0001_pon"
-    mat_file = os.path.join(mat_dir, file_name + ".mat")
-    mat_loader = MatLoader(mat_file)
-    on_images = mat_loader.images
-
-    print(f"{off_images.shape = }")
-    print(f"{on_images.shape = }")
+    npz_file = os.path.join(config.path.npz_dir, "run=0144_scan=0001.npz")
+    data: dict = NpzLoader(npz_file).data
+    print(list(data.keys()))
